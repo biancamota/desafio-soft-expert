5a975b213aa292b35cebc8ef44e3bfd376c83e3b
Bianca Mota <biancamota1209@gmail.com>
2023-04-11 21:49:31 -0300

    delete files

--- app/Http/Router.php
+++ app/Http/Router.php
@@ -24,43 +24,5 @@ class Router
 
     public function dispatch(Request $request)
     {
-        $matchedRoute = null;
-    
-        foreach ($this->routes as $route) {
-            if ($route['method'] !== $request->getHttpMethod()) {
-                continue;
-            }
-            
-            $routePath = preg_replace('/\//', '\\/', $route['path']);
-            $routePath = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $routePath);
-            $routePath = '/^' . $routePath . '\/?([0-9]+)?\/?$/';
-            
-            if (preg_match($routePath, $request->getUri(), $matches)) {
-                $params = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
-                $action = explode('@', $route['action']);
-                
-                if (count($action) !== 2) {
-                    throw new \Exception('Invalid route action: ' . $route['action'], 500);
-                }
-    
-                $controllerName = $action[0];
-                $controllerMethod = $action[1];
-    
-                $controller = new $controllerName();
-    
-                $matchedRoute = [
-                    'controller' => $controller,
-                    'method' => $controllerMethod,
-                    'params' => $params,
-                ];
-                break;
-            }
-        }
-    
-        if (!$matchedRoute) {
-            throw new \Exception('Route not found', 404);
-        }
-    
-        return $matchedRoute['controller']->$matchedRoute['method']($request, $matchedRoute['params']);
     }
 }

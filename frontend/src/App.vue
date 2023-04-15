c641919af797945c1731d62f3d8cbbd38b3807a2
Bianca Mota <biancamota1209@gmail.com>
2023-04-14 22:31:37 -0300

    feat: add front

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-link :to="{name: 'products'}">Products</router-link>
+  <router-link :to="{name: 'login'}">Login</router-link>
+  <router-link :to="{name: 'sales'}">Sales</router-link>
+
+  <router-view/>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
